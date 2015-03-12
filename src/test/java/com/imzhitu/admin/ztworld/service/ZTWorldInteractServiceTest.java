7d58ccd8ea65b5dbef63ef06e6b089d34bc14e02
zengxianxian <474183700@qq.com>
2015-03-12 14:20:26 +0800

    添加一个测试

---
+++ src/test/java/com/imzhitu/admin/ztworld/service/ZTWorldInteractServiceTest.java
@@ -0,0 +1,19 @@
+package com.imzhitu.admin.ztworld.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hts.web.base.constant.Tag;
+import com.imzhitu.admin.base.BaseTest;
+
+public class ZTWorldInteractServiceTest extends BaseTest{
+	
+	@Autowired
+	private ZTWorldInteractService service;
+	
+	@Test
+	public void shieldCommentByUserIdTest()throws Exception{
+		service.updateCommentShieldByUserId(1592, Tag.TRUE);
+	}
+
+}
