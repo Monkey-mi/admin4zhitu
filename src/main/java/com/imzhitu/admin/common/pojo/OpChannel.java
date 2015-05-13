2c187e10e0c4a65867d70f75586e9dcfc004f2d4
lynch <zhutianjie89@gmail.com>
2015-05-13 17:56:42 +0800

    修改OpChannel映射

--- src/main/java/com/imzhitu/admin/common/pojo/OpChannel.java
+++ src/main/java/com/imzhitu/admin/common/pojo/OpChannel.java
@@ -9,14 +9,26 @@ public class OpChannel extends AbstractNumberDto {
 	 */
 	private static final long serialVersionUID = 4105884798332317238L;
 	private Integer id;
+	private Integer ownerId;
 	private String channelName;
 	private String channelTitle;
+	private String subtitle;
+	private String channelDesc;
 	private String channelIcon;
 	private String subIcon;
+	private Integer channelType;
+	private Integer worldCount;
 	private Integer childCount;
+	private Integer memberCount;
+	private Integer superbCount;
+	private Long createTime;
+	private Long lastModified;
 	private Integer childCountBase;
 	private Integer valid;
 	private Integer serial;
+	private Integer danmu;
+	private Integer mood;
+	private Integer world;
 
 	public OpChannel() {
 		super();
@@ -92,6 +104,102 @@ public class OpChannel extends AbstractNumberDto {
 
 	public void setSubIcon(String subIcon) {
 		this.subIcon = subIcon;
+	}
+
+	public Integer getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(Integer ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getSubtitle() {
+		return subtitle;
+	}
+
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+
+	public String getChannelDesc() {
+		return channelDesc;
+	}
+
+	public void setChannelDesc(String channelDesc) {
+		this.channelDesc = channelDesc;
+	}
+
+	public Integer getChannelType() {
+		return channelType;
+	}
+
+	public void setChannelType(Integer channelType) {
+		this.channelType = channelType;
+	}
+
+	public Integer getWorldCount() {
+		return worldCount;
+	}
+
+	public void setWorldCount(Integer worldCount) {
+		this.worldCount = worldCount;
+	}
+
+	public Integer getMemberCount() {
+		return memberCount;
+	}
+
+	public void setMemberCount(Integer memberCount) {
+		this.memberCount = memberCount;
+	}
+
+	public Integer getSuperbCount() {
+		return superbCount;
+	}
+
+	public void setSuperbCount(Integer superbCount) {
+		this.superbCount = superbCount;
+	}
+
+	public Long getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Long createTime) {
+		this.createTime = createTime;
+	}
+
+	public Long getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(Long lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	public Integer getDanmu() {
+		return danmu;
+	}
+
+	public void setDanmu(Integer danmu) {
+		this.danmu = danmu;
+	}
+
+	public Integer getMood() {
+		return mood;
+	}
+
+	public void setMood(Integer mood) {
+		this.mood = mood;
+	}
+
+	public Integer getWorld() {
+		return world;
+	}
+
+	public void setWorld(Integer world) {
+		this.world = world;
 	}
 	
 }
