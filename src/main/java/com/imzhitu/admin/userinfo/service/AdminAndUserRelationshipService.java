e1768fe7a76c561fd85fd7441af403b835c0ed39
BobPicWeaver <benben683280@126.com>
2015-05-15 12:05:24 +0800

    新增接口方法

--- src/main/java/com/imzhitu/admin/userinfo/service/AdminAndUserRelationshipService.java
+++ src/main/java/com/imzhitu/admin/userinfo/service/AdminAndUserRelationshipService.java
@@ -1,8 +1,10 @@
 package com.imzhitu.admin.userinfo.service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.hts.web.common.service.BaseService;
+import com.imzhitu.admin.common.pojo.AdminAndUserRelationshipDto;
 
 /**
  * 管理员账号与织图用户绑定关系业务逻辑接口
@@ -44,6 +46,18 @@ public interface AdminAndUserRelationshipService extends BaseService {
      * @author zhangbo 2015-05-13
      */
     public Map<String, Object> queryAdminAndUserRelationship(Integer adminId) throws Exception;
+    
+    /**
+     * 查询管理员账号与织图用户之间的关联关系
+     * 1、若当前传入用户为超级管理员（super_admin）或运营管理员（op_admin），则返回全表数据
+     * 2、若当前传入用户为普通管理员，则返回名下当前管理员关联的织图用户
+     *
+     * @param adminId	管理员账号ID
+     * @return
+     * @throws Exception
+     * @author zhangbo 2015年5月15日
+     */
+    public List<AdminAndUserRelationshipDto> getAllAdminAndUserRelationshipByRole(Integer adminId) throws Exception;
     
     /**
      * 删除管理员账号与织图用户之间的关联关系，为硬删除
