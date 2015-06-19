26f6f96880f95a27f76495a9c462912f90a1489f
lynch <zhutianjie89@gmail.com>
2015-06-19 11:03:26 +0800

    把写操作放到主库

--- src/main/java/com/imzhitu/admin/op/mapper/OpMsgBulletinMapper.java
+++ src/main/java/com/imzhitu/admin/op/mapper/OpMsgBulletinMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.imzhitu.admin.common.dataSourceMasterSlave.DataSource;
 import com.imzhitu.admin.common.pojo.OpMsgBulletin;
 
 /**
@@ -16,24 +17,28 @@ public interface OpMsgBulletinMapper {
 	 * 添加
 	 * @param dto
 	 */
+	@DataSource("master")
 	public void insertMsgBulletin(OpMsgBulletin dto);
 	
 	/**
 	 * 批量删除
 	 * @param ids
 	 */
+	@DataSource("master")
 	public void batchDeleteMsgBulletin(Integer[] ids);
 	
 	/**
 	 * 更新
 	 * @param dto
 	 */
+	@DataSource("master")
 	public void updateMsgBulletin(OpMsgBulletin dto);
 	
 	/**
 	 * 批量更新有效性
 	 * @param ids
 	 */
+	@DataSource("master")
 	public void batchUpdateMsgBulletinValid(@Param("ids")Integer[] ids,@Param("valid")Integer valid,@Param("operator")Integer operator);
 	
 	/**
@@ -41,6 +46,7 @@ public interface OpMsgBulletinMapper {
 	 * @param dto
 	 * @return
 	 */
+	@DataSource("slave")
 	public List<OpMsgBulletin> queryMsgBulletin(OpMsgBulletin dto);
 	
 	/**
@@ -48,6 +54,7 @@ public interface OpMsgBulletinMapper {
 	 * @param dto
 	 * @return
 	 */
+	@DataSource("slave")
 	public long queryMsgBulletinTotalCount(OpMsgBulletin dto);
 	
 	/**
@@ -55,5 +62,6 @@ public interface OpMsgBulletinMapper {
 	 * @param ids
 	 * @return
 	 */
+	@DataSource("slave")
 	public List<OpMsgBulletin> queryMsgBulletinByIds(@Param("ids")Integer[] ids);
 }
--- src/main/java/com/imzhitu/admin/op/mapper/OpMsgStartpageMapper.java
+++ src/main/java/com/imzhitu/admin/op/mapper/OpMsgStartpageMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.imzhitu.admin.common.dataSourceMasterSlave.DataSource;
 import com.imzhitu.admin.common.pojo.OpMsgStartpage;
 
 /**
@@ -16,24 +17,28 @@ public interface OpMsgStartpageMapper {
 	 * 添加
 	 * @param dto
 	 */
+	@DataSource("master")
 	public void insertMsgStartpage(OpMsgStartpage dto);
 	
 	/**
 	 * 批量删除
 	 * @param ids
 	 */
+	@DataSource("master")
 	public void batchDeleteMsgStartpage(Integer[] ids);
 	
 	/**
 	 * 更新
 	 * @param dto
 	 */
+	@DataSource("master")
 	public void updateMsgStartpage(OpMsgStartpage dto);
 	
 	/**
 	 * 批量更新有效性
 	 * @param ids
 	 */
+	@DataSource("master")
 	public void batchUpdateMsgStartpageValid(@Param("ids")Integer[] ids,@Param("valid")Integer valid,@Param("operator")Integer operator);
 	
 	/**
@@ -41,6 +46,7 @@ public interface OpMsgStartpageMapper {
 	 * @param dto
 	 * @return
 	 */
+	@DataSource("slave")
 	public List<OpMsgStartpage> queryMsgStartpage(OpMsgStartpage dto);
 	
 	/**
@@ -48,6 +54,7 @@ public interface OpMsgStartpageMapper {
 	 * @param dto
 	 * @return
 	 */
+	@DataSource("slave")
 	public long queryMsgStartpageTotalCount(OpMsgStartpage dto);
 	
 	/**
@@ -55,11 +62,13 @@ public interface OpMsgStartpageMapper {
 	 * @param ids
 	 * @return
 	 */
+	@DataSource("slave")
 	public List<OpMsgStartpage> queryMsgBulletinByIds(@Param("ids")Integer[] ids);
 	
 	/**
 	 * 批量更新lastmodifie
 	 * @param ids
 	 */
+	@DataSource("master")
 	public void batchUpdateMsgStartpageLastModified(Integer[] ids);
 }
