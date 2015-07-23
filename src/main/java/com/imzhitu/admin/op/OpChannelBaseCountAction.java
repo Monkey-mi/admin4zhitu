6a3ed3713b1b24e80a42edd79ca07cbba2e8caa7
BobPicWeaver <benben683280@126.com>
2015-07-23 15:35:31 +0800

    加精与置顶操作刷新逻辑下沉

--- src/main/java/com/imzhitu/admin/op/OpChannelBaseCountAction.java
+++ src/main/java/com/imzhitu/admin/op/OpChannelBaseCountAction.java
@@ -23,145 +23,130 @@ import com.imzhitu.admin.op.service.OpChannelBaseCountService;
  */
 public class OpChannelBaseCountAction extends BaseCRUDAction {
 
-	/**
-	 * 序列号
-	 */
-	private static final long serialVersionUID = 6813398937925814369L;
+    /**
+     * 序列号
+     */
+    private static final long serialVersionUID = 6813398937925814369L;
 
-	private Logger log = Logger.getLogger(OpChannelBaseCountAction.class);
+    private Logger log = Logger.getLogger(OpChannelBaseCountAction.class);
 
-	private ChannelBaseCountDto baseCountDto = new ChannelBaseCountDto();
+    private ChannelBaseCountDto baseCountDto = new ChannelBaseCountDto();
 
-	private String channelIds;
+    private String channelIds;
 
-	/**
-	 * @return the baseCountDto
-	 */
-	public ChannelBaseCountDto getBaseCountDto() {
-		return baseCountDto;
+    /**
+     * @return the baseCountDto
+     */
+    public ChannelBaseCountDto getBaseCountDto() {
+	return baseCountDto;
+    }
+
+    /**
+     * @param baseCountDto
+     *            the baseCountDto to set
+     */
+    public void setBaseCountDto(ChannelBaseCountDto baseCountDto) {
+	this.baseCountDto = baseCountDto;
+    }
+
+    /**
+     * @return the channelIds
+     */
+    public String getChannelIds() {
+	return channelIds;
+    }
+
+    /**
+     * @param channelIds
+     *            the channelIds to set
+     */
+    public void setChannelIds(String channelIds) {
+	this.channelIds = channelIds;
+    }
+
+    @Autowired
+    private OpChannelBaseCountService service;
+
+    @Autowired
+    private ChannelService channelService;
+
+    /**
+     * 保存数据到频道基数管理表
+     *
+     * @return
+     * @author zhangbo 2015年5月30日
+     */
+    public String saveChannelBaseCount() {
+	try {
+	    if (isExistChannel(baseCountDto.getChannelId())) {
+		service.saveChannelBaseCount(baseCountDto);
+
+		JSONUtil.optSuccess(OptResult.ADD_SUCCESS, jsonMap);
+	    } else {
+		JSONUtil.optSuccess("频道id不存在，请填写正确的频道id", jsonMap);
+	    }
+	} catch (Exception e) {
+	    log.error(e);
+	    JSONUtil.optFailed(OptResult.ADD_FAILED, jsonMap);
+	    ;
 	}
+	return StrutsKey.JSON;
+    }
 
-	/**
-	 * @param baseCountDto
-	 *            the baseCountDto to set
-	 */
-	public void setBaseCountDto(ChannelBaseCountDto baseCountDto) {
-		this.baseCountDto = baseCountDto;
+    /**
+     * 查询频道基数页面表格展示数据
+     *
+     * @return
+     * @author zhangbo 2015年5月30日
+     */
+    public String buildChannelBaseCountList() {
+	try {
+	    // 设置当前登陆人员，进行名下查询
+	    baseCountDto.setAdminId(getCurrentLoginUserId());
+	    service.buildChannelBaseCountList(baseCountDto, page, rows, jsonMap);
+	    JSONUtil.optSuccess(OptResult.QUERY_SUCCESS, jsonMap);
+	} catch (Exception e) {
+	    log.error(e);
+	    JSONUtil.optFailed(OptResult.QUERY_FAILED, jsonMap);
+	    ;
 	}
+	return StrutsKey.JSON;
+    }
 
-	/**
-	 * @return the channelIds
-	 */
-	public String getChannelIds() {
-		return channelIds;
+    /**
+     * 批量删除数据到频道基数管理表
+     *
+     * @return
+     * @author zhangbo 2015年5月30日
+     */
+    public String deleteChannelBaseCount() {
+	try {
+	    Integer[] ids = StringUtil.convertStringToIds(getChannelIds());
+	    service.deleteChannelBaseCount(ids);
+	    JSONUtil.optSuccess(OptResult.DELETE_SUCCESS, jsonMap);
+	} catch (Exception e) {
+	    log.error(e);
+	    JSONUtil.optFailed(OptResult.DELETE_SUCCESS, jsonMap);
+	    ;
 	}
+	return StrutsKey.JSON;
+    }
 
-	/**
-	 * @param channelIds
-	 *            the channelIds to set
-	 */
-	public void setChannelIds(String channelIds) {
-		this.channelIds = channelIds;
+    /**
+     * 判断频道是否存在
+     *
+     * @param channelId
+     * @return
+     * @throws Exception
+     * @author zhangbo 2015年6月1日
+     */
+    private boolean isExistChannel(Integer channelId) throws Exception {
+	OpChannel queryChannelById = channelService.queryChannelById(channelId);
+	if (queryChannelById == null) {
+	    return false;
+	} else {
+	    return true;
 	}
-
-	@Autowired
-	private OpChannelBaseCountService service;
-
-	@Autowired
-	private ChannelService channelService;
-
-	@Autowired
-	private com.hts.web.operations.service.ChannelService webChannelService;
-
-	/**
-	 * 保存数据到频道基数管理表
-	 *
-	 * @return
-	 * @author zhangbo 2015年5月30日
-	 */
-	public String saveChannelBaseCount() {
-		try {
-			if (isExistChannel(baseCountDto.getChannelId())) {
-				service.saveChannelBaseCount(baseCountDto);
-
-				webChannelService.updateWorldAndChildCount(baseCountDto.getChannelId()); // 更新织图和图片总数
-				webChannelService.updateMemberCount(baseCountDto.getChannelId()); // 更新频道成员订阅总数
-				webChannelService.updateSuperbCount(baseCountDto.getChannelId()); // 更新频道加精总数
-
-				JSONUtil.optSuccess(OptResult.ADD_SUCCESS, jsonMap);
-			} else {
-				JSONUtil.optSuccess("频道id不存在，请填写正确的频道id", jsonMap);
-			}
-		} catch (Exception e) {
-			log.error(e);
-			JSONUtil.optFailed(OptResult.ADD_FAILED, jsonMap);
-			;
-		}
-		return StrutsKey.JSON;
-	}
-
-	/**
-	 * 查询频道基数页面表格展示数据
-	 *
-	 * @return
-	 * @author zhangbo 2015年5月30日
-	 */
-	public String buildChannelBaseCountList() {
-		try {
-			// 设置当前登陆人员，进行名下查询
-			baseCountDto.setAdminId(getCurrentLoginUserId());
-			service.buildChannelBaseCountList(baseCountDto, page, rows, jsonMap);
-			JSONUtil.optSuccess(OptResult.QUERY_SUCCESS, jsonMap);
-		} catch (Exception e) {
-			log.error(e);
-			JSONUtil.optFailed(OptResult.QUERY_FAILED, jsonMap);
-			;
-		}
-		return StrutsKey.JSON;
-	}
-
-	/**
-	 * 批量删除数据到频道基数管理表
-	 *
-	 * @return
-	 * @author zhangbo 2015年5月30日
-	 */
-	public String deleteChannelBaseCount() {
-		try {
-			Integer[] ids = StringUtil.convertStringToIds(getChannelIds());
-			service.deleteChannelBaseCount(ids);
-
-			for (Integer channelId : ids) {
-				webChannelService.updateWorldAndChildCount(channelId); // 更新织图和图片总数
-				webChannelService.updateMemberCount(channelId); // 更新频道成员订阅总数
-				webChannelService
-						.updateSuperbCount(baseCountDto.getChannelId()); // 更新频道加精总数
-			}
-
-			JSONUtil.optSuccess(OptResult.DELETE_SUCCESS, jsonMap);
-		} catch (Exception e) {
-			log.error(e);
-			JSONUtil.optFailed(OptResult.DELETE_SUCCESS, jsonMap);
-			;
-		}
-		return StrutsKey.JSON;
-	}
-
-	/**
-	 * 判断频道是否存在
-	 *
-	 * @param channelId
-	 * @return
-	 * @throws Exception
-	 * @author zhangbo 2015年6月1日
-	 */
-	private boolean isExistChannel(Integer channelId) throws Exception {
-		OpChannel queryChannelById = channelService.queryChannelById(channelId);
-		if (queryChannelById == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    }
 
 }
--- src/main/java/com/imzhitu/admin/op/service/impl/OpChannelBaseCountServiceImpl.java
+++ src/main/java/com/imzhitu/admin/op/service/impl/OpChannelBaseCountServiceImpl.java
@@ -22,90 +22,92 @@ import com.imzhitu.admin.privileges.dao.RoleDao;
  * @author zhangbo 2015年5月29日
  */
 @Service
-public class OpChannelBaseCountServiceImpl extends BaseServiceImpl implements
-		OpChannelBaseCountService {
+public class OpChannelBaseCountServiceImpl extends BaseServiceImpl implements OpChannelBaseCountService {
 
-	@Autowired
-	private OpChannelBaseCountMapper mapper;
+    @Autowired
+    private OpChannelBaseCountMapper mapper;
 
-	@Autowired
-	private RoleDao roleDao;
+    @Autowired
+    private RoleDao roleDao;
 
-	@Override
-	public void saveChannelBaseCount(ChannelBaseCountDto dto) throws Exception {
-		// 若存在，更新，不存在，插入
-		if (isExist(dto.getChannelId())) {
-			mapper.updateChannelBaseCount(dto);
-		} else {
-			mapper.insertChannelBaseCount(dto);
+    @Autowired
+    private com.hts.web.operations.service.ChannelService webChannelService;
+
+    @Override
+    public void saveChannelBaseCount(ChannelBaseCountDto dto) throws Exception {
+	// 若存在，更新，不存在，插入
+	if (isExist(dto.getChannelId())) {
+	    mapper.updateChannelBaseCount(dto);
+	} else {
+	    mapper.insertChannelBaseCount(dto);
+	}
+
+	webChannelService.updateWorldAndChildCount(dto.getChannelId()); // 更新织图和图片总数
+	webChannelService.updateMemberCount(dto.getChannelId()); // 更新频道成员订阅总数
+	webChannelService.updateSuperbCount(dto.getChannelId()); // 更新频道加精总数
+    }
+
+    @Override
+    public void buildChannelBaseCountList(ChannelBaseCountDto dto, Integer page, Integer rows,
+	    Map<String, Object> jsonMap) throws Exception {
+	buildNumberDtos("getChannelId", dto, page, rows, jsonMap, new NumberDtoListAdapter<ChannelBaseCountDto>() {
+
+	    @Override
+	    public List<? extends Serializable> queryList(ChannelBaseCountDto dto) {
+		if (roleDao.isSuperOrOpAdmin(dto.getAdminId())) {
+		    // 若为super_admin或op_admin权限，则把adminId置空，进行全表查询
+		    dto.setAdminId(null);
 		}
-	}
+		List<ChannelBaseCountDto> resultList = mapper.queryChannelBaseCountList(dto);
 
-	@Override
-	public void buildChannelBaseCountList(ChannelBaseCountDto dto,
-			Integer page, Integer rows, Map<String, Object> jsonMap)
-			throws Exception {
-		buildNumberDtos("getChannelId", dto, page, rows, jsonMap,
-				new NumberDtoListAdapter<ChannelBaseCountDto>() {
-
-					@Override
-					public List<? extends Serializable> queryList(
-							ChannelBaseCountDto dto) {
-						if (roleDao.isSuperOrOpAdmin(dto.getAdminId())) {
-							// 若为super_admin或op_admin权限，则把adminId置空，进行全表查询
-							dto.setAdminId(null);
-						}
-						List<ChannelBaseCountDto> resultList = mapper
-								.queryChannelBaseCountList(dto);
-
-						// 后台查询出的trueXX字段，为展示在客户端给用户看的数据，真实数据要减去base值，故做下列循环操作，方便运营端查看真实数据
-						for (ChannelBaseCountDto cbcDto : resultList) {
-							cbcDto.setTrueWorldCount(cbcDto.getTrueWorldCount()
-									- cbcDto.getWorldBaseCount());
-							cbcDto.setTrueChildCount(cbcDto.getTrueChildCount()
-									- cbcDto.getChildBaseCount());
-							cbcDto.setTrueMemberCount(cbcDto
-									.getTrueMemberCount()
-									- cbcDto.getMemberBaseCount());
-							cbcDto.setTrueSuperbCount(cbcDto
-									.getTrueSuperbCount()
-									- cbcDto.getSuperbBaseCount());
-						}
-						return resultList;
-					}
-
-					@Override
-					public long queryTotal(ChannelBaseCountDto dto) {
-						if (roleDao.isSuperOrOpAdmin(dto.getAdminId())) {
-							// 若为super_admin或op_admin权限，则把adminId置空，进行全表查询
-							dto.setAdminId(null);
-						}
-						return mapper.queryChannelBaseCount(dto);
-					}
-				});
-	}
-
-	@Override
-	public void deleteChannelBaseCount(Integer[] channelIds) throws Exception {
-		mapper.deleteChannelBaseCountByChannelIds(channelIds);
-	}
-
-	/**
-	 * 判断是否存在于频道基数管理表
-	 *
-	 * @param channelId
-	 *            频道id
-	 * @return
-	 * @throws Exception
-	 * @author zhangbo 2015年6月1日
-	 */
-	private boolean isExist(Integer channelId) throws Exception {
-		long resultCount = mapper.isExistChannelBaseCount(channelId);
-		if (resultCount == 0) {
-			return false;
-		} else {
-			return true;
+		// 后台查询出的trueXX字段，为展示在客户端给用户看的数据，真实数据要减去base值，故做下列循环操作，方便运营端查看真实数据
+		for (ChannelBaseCountDto cbcDto : resultList) {
+		    cbcDto.setTrueWorldCount(cbcDto.getTrueWorldCount() - cbcDto.getWorldBaseCount());
+		    cbcDto.setTrueChildCount(cbcDto.getTrueChildCount() - cbcDto.getChildBaseCount());
+		    cbcDto.setTrueMemberCount(cbcDto.getTrueMemberCount() - cbcDto.getMemberBaseCount());
+		    cbcDto.setTrueSuperbCount(cbcDto.getTrueSuperbCount() - cbcDto.getSuperbBaseCount());
 		}
+		return resultList;
+	    }
+
+	    @Override
+	    public long queryTotal(ChannelBaseCountDto dto) {
+		if (roleDao.isSuperOrOpAdmin(dto.getAdminId())) {
+		    // 若为super_admin或op_admin权限，则把adminId置空，进行全表查询
+		    dto.setAdminId(null);
+		}
+		return mapper.queryChannelBaseCount(dto);
+	    }
+	});
+    }
+
+    @Override
+    public void deleteChannelBaseCount(Integer[] channelIds) throws Exception {
+	mapper.deleteChannelBaseCountByChannelIds(channelIds);
+
+	for (Integer channelId : channelIds) {
+	    webChannelService.updateWorldAndChildCount(channelId); // 更新织图和图片总数
+	    webChannelService.updateMemberCount(channelId); // 更新频道成员订阅总数
+	    webChannelService.updateSuperbCount(channelId); // 更新频道加精总数
 	}
+    }
+
+    /**
+     * 判断是否存在于频道基数管理表
+     *
+     * @param channelId
+     *            频道id
+     * @return
+     * @throws Exception
+     * @author zhangbo 2015年6月1日
+     */
+    private boolean isExist(Integer channelId) throws Exception {
+	long resultCount = mapper.isExistChannelBaseCount(channelId);
+	if (resultCount == 0) {
+	    return false;
+	} else {
+	    return true;
+	}
+    }
 
 }
