00783993e8462ea93a762ca4551b8dc99c6ed759
mike <mike@mike-Inspiron-5548>
2015-08-06 18:25:33 +0800

    增加获取实现二级分类方法

---
+++ src/test/java/com/imzhitu/admin/interact/service/impl/InteractCommentLableIdServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.imzhitu.admin.interact.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.imzhitu.admin.base.BaseTest;
+import com.imzhitu.admin.interact.service.InteractCommentLabelIdService;
+
+public class InteractCommentLableIdServiceImplTest extends BaseTest {
+	
+	@Autowired
+	private InteractCommentLabelIdService service;
+
+	@Test
+	public void testInsertLableId() {
+		Integer lableId = 123123123;
+		try {
+			System.out.println("=======>" + service);
+			service.insertLableId(lableId);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
