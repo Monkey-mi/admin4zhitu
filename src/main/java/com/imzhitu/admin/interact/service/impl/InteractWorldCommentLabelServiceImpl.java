b3c13f40b69b71d4a142ee51cefade27a3a46f94
zengxianxian <474183700@qq.com>
2015-08-07 16:20:35 +0800

    添加标签时，增加互动

--- src/main/java/com/imzhitu/admin/interact/service/impl/InteractWorldCommentLabelServiceImpl.java
+++ src/main/java/com/imzhitu/admin/interact/service/impl/InteractWorldCommentLabelServiceImpl.java
@@ -9,20 +9,28 @@ import com.hts.web.common.service.impl.BaseServiceImpl;
 import com.imzhitu.admin.common.pojo.InteractWorldCommentLabel;
 import com.imzhitu.admin.interact.mapper.InteractWorldCommentLabelMapper;
 import com.imzhitu.admin.interact.service.InteractWorldCommentLabelService;
+import com.imzhitu.admin.interact.service.InteractWorldService;
 
 @Service
 public class InteractWorldCommentLabelServiceImpl extends BaseServiceImpl implements InteractWorldCommentLabelService{
 	
 	@Autowired
 	private InteractWorldCommentLabelMapper worldCommentLabelMapper;
+	
+	@Autowired
+	private InteractWorldService worldService;
 
 	@Override
 	public void insertWorldCommentLabel(Integer worldId, String labelIds, Integer operator) throws Exception {
+		if(worldId == null || labelIds == null || "".equals(labelIds.trim())){
+			throw new Exception("worldId ="+worldId+"\nlabelIds="+labelIds);
+		}
 		InteractWorldCommentLabel dto = new InteractWorldCommentLabel();
 		dto.setWorldId(worldId);
 		dto.setLabelIds(labelIds);
 		dto.setOperator(operator);
 		worldCommentLabelMapper.insertWorldCommentLabel(dto);
+		worldService.saveInteractV3(worldId, labelIds);
 	}
 
 	@Override
--- src/main/java/com/imzhitu/admin/interact/service/impl/InteractWorldServiceImpl.java
+++ src/main/java/com/imzhitu/admin/interact/service/impl/InteractWorldServiceImpl.java
@@ -33,6 +33,7 @@ import com.imzhitu.admin.common.pojo.InteractWorldClick;
 import com.imzhitu.admin.common.pojo.InteractWorldCommentDto;
 import com.imzhitu.admin.common.pojo.InteractWorldLiked;
 import com.imzhitu.admin.common.pojo.OpZombieDegreeUserLevel;
+import com.imzhitu.admin.common.pojo.UserLevelDto;
 import com.imzhitu.admin.common.pojo.UserLevelListDto;
 import com.imzhitu.admin.common.service.KeyGenService;
 import com.imzhitu.admin.interact.dao.InteractCommentDao;
@@ -46,6 +47,7 @@ import com.imzhitu.admin.interact.mapper.InteractWorldCommentMapper;
 import com.imzhitu.admin.interact.mapper.InteractWorldLikedMapper;
 import com.imzhitu.admin.interact.service.InteractCommentService;
 import com.imzhitu.admin.interact.service.InteractUserlevelListService;
+import com.imzhitu.admin.interact.service.InteractUserlevelService;
 import com.imzhitu.admin.interact.service.InteractWorldService;
 import com.imzhitu.admin.op.dao.UserZombieDao;
 import com.imzhitu.admin.op.mapper.OpZombieMapper;
@@ -196,6 +198,9 @@ public class InteractWorldServiceImpl extends BaseServiceImpl implements
 	
 	@Autowired
 	private InteractUserlevelListService userLevelListService;
+	
+	@Autowired
+	private InteractUserlevelService userLevelService;
 	
 	@Autowired
 	private OpZombieDegreeUserLevelService zombieDegreeUserLevelService;
@@ -1631,6 +1636,8 @@ public class InteractWorldServiceImpl extends BaseServiceImpl implements
 		Integer needZombieDegreeId = null;
 		if( null != userLevelDto){
 			try{
+				//如果clickCount、likeCount为空，则采用userlevel里的值
+				
 				List<OpZombieDegreeUserLevel> zombieDegreeUserLevelList = zombieDegreeUserLevelService.queryZombieDegree(null, null, userLevelDto.getUser_level_id());
 				if(zombieDegreeUserLevelList != null && zombieDegreeUserLevelList.size() == 1){
 					needZombieDegreeId = zombieDegreeUserLevelList.get(0).getZombieDegreeId();
@@ -1645,6 +1652,87 @@ public class InteractWorldServiceImpl extends BaseServiceImpl implements
 		}
 		saveInteractV3(uid,needZombieDegreeId,worldId,clickCount,likeCount,commentIds,minuteDuration);
 	}
+	
+	
+	
+	/**
+	 * 根据织图id、织图标签，进行用户等级的互动，
+	 * @param worldId
+	 * @param labelIdsStr
+	 * @throws Exception
+	 */
+	@Override
+	public void saveInteractV3(Integer worldId ,String labelIdsStr)throws Exception{
+		Integer uid = interactWorldlevelDao.QueryUIDByWID(worldId);
+		UserLevelListDto userLevelDto = userLevelListService.QueryUserlevelByUserId(uid);
+		Integer needZombieDegreeId = null;
+		Integer clickCount = 0;
+		Integer likeCount = 0;
+		Integer commentCount = 0;
+		Integer minuteDuration = 0;
+		String[] commentIds = null;
+		if( null != userLevelDto){
+			try{
+				//获取用户等级里面的值
+				UserLevelDto userLevel = userLevelService.QueryUserlevelById(userLevelDto.getUser_level_id());
+				clickCount = userLevel.getMin_play_times() + (int)(Math.round(Math.random()*(userLevel.getMax_play_times()-userLevel.getMin_play_times())));
+				likeCount  = userLevel.getMin_liked_count() + (int)(Math.round(Math.random()*(userLevel.getMax_liked_count() - userLevel.getMin_liked_count())));
+				commentCount = userLevel.getMin_comment_count() + (int)(Math.round(Math.random()*(userLevel.getMax_comment_count() - userLevel.getMin_comment_count())));
+				minuteDuration = userLevel.getTime();
+				
+				//从对应的标签中获取评论
+				StringBuilder sb = new StringBuilder();
+				Integer [] labelIds = StringUtil.convertStringToIds(labelIdsStr);
+				Integer avetege = (int)(commentCount / labelIds.length);
+				avetege = avetege > 0 ? avetege : 1;
+				
+				for( int i=0; i<labelIds.length; i++){
+					if(commentCount >= avetege){
+						commentCount -= avetege;
+						List<Integer> commentList = interactCommentService.getRandomCommentIds(labelIds[i], avetege);
+						if(sb.length() > 0){
+							sb.append(',');
+						}
+						if (commentList != null && commentList.size() > 0) {
+						    String commentStrs = commentList.toString();
+						    sb.append(commentStrs.substring(1, commentStrs.length()-1)); 
+						}
+					}else if( commentCount > 0){
+						List<Integer> commentList = interactCommentService.getRandomCommentIds(labelIds[i], (int)commentCount);
+						if(sb.length() > 0){
+							sb.append(',');
+						}
+						if (commentList != null && commentList.size() > 0) {
+						    String commentStrs = commentList.toString();
+						    sb.append(commentStrs.substring(1, commentStrs.length()-1)); 
+						}
+						break;
+					}else{
+						break;
+					}
+				}
+				
+				if(sb.length() > 0){
+					String commentStr = sb.toString();
+					commentIds = commentStr.split(",");
+				}
+				
+				//获取用户对应的马甲等级
+				List<OpZombieDegreeUserLevel> zombieDegreeUserLevelList = zombieDegreeUserLevelService.queryZombieDegree(null, null, userLevelDto.getUser_level_id());
+				if(zombieDegreeUserLevelList != null && zombieDegreeUserLevelList.size() == 1){
+					needZombieDegreeId = zombieDegreeUserLevelList.get(0).getZombieDegreeId();
+				}else{
+					needZombieDegreeId = commonZombieDegreeId;
+				}
+			}catch(Exception e){
+				needZombieDegreeId = commonZombieDegreeId;
+			}
+		}else{
+			throw new Exception("saveInteractV3:userLevelListService.QueryUserlevelByUserId is null.\nuserId="+uid);
+		}
+		saveInteractV3(uid,needZombieDegreeId,worldId,clickCount,likeCount,commentIds,minuteDuration);
+	}
+	
 	
 	@Override
 	public void saveChannelInteractV3(Integer channelId,Integer worldId,Integer clickCount,
