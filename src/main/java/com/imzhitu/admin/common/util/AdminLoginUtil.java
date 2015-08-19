7a053ab399a0b2f0fef4204e2c1783415757c5e4
lynch <zhutianjie89@gmail.com>
2015-08-19 18:00:01 +0800

    分离获取当前登陆用户接口到工具方法

---
+++ src/main/java/com/imzhitu/admin/common/util/AdminLoginUtil.java
@@ -0,0 +1,32 @@
+package com.imzhitu.admin.common.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.imzhitu.admin.common.pojo.AdminUserDetails;
+
+/**
+ * <p>
+ * 登陆工具类
+ * </p>
+ * 
+ * @author lynch
+ *
+ */
+public class AdminLoginUtil {
+
+	/**
+	 * 获取当前登陆用户id
+	 * 
+	 * @return
+	 * @author lynch
+	 */
+	public static Integer getCurrentLoginId() {
+		Integer uid = -1;
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			uid = ((AdminUserDetails) auth.getPrincipal()).getId();
+		}
+		return uid;
+	}
+}
