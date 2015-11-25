0715016ee3379eda4caee2283e6dbf64f7ba61d4
Monkey-mi <mishengliang@live.com>
2015-11-25 17:25:13 +0800

    ZTworld pojo

---
+++ src/main/java/com/imzhitu/admin/ztworld/pojp/ZTWorld.java
@@ -0,0 +1,13 @@
+package com.imzhitu.admin.ztworld.pojp;
+
+import java.io.Serializable;
+
+public class ZTWorld implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8407087511918773995L;
+
+	
+}
