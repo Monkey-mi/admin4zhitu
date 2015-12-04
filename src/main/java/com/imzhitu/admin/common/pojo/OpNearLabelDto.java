72e740a784a647963fce82bbd0db49e21f7fbea7
zengxianxian <474183700@qq.com>
2015-12-04 14:21:48 +0800

    新增附近模块

---
+++ src/main/java/com/imzhitu/admin/common/pojo/OpNearLabelDto.java
@@ -0,0 +1,98 @@
+package com.imzhitu.admin.common.pojo;
+
+import com.hts.web.common.pojo.AbstractNumberDto;
+
+public class OpNearLabelDto extends AbstractNumberDto{
+	private static final long serialVersionUID = -2650022252782052499L;
+	private Integer id;
+	private Integer cityId;
+	private String cityName;
+	private String labelName;
+	private Double longitude;
+	private Double latitude;
+	private String desc;
+	private String bannerUrl;
+	private Integer serial;
+	private Integer maxId = 0;
+	private Integer firstRow; // 起始位置
+	private Integer limit;
+	
+	public Integer getMaxId() {
+		return maxId;
+	}
+
+	public void setMaxId(Integer maxId) {
+		this.maxId = maxId;
+	}
+
+	public Integer getLimit() {
+		return limit;
+	}
+
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+
+	public Integer getFirstRow() {
+		return firstRow;
+	}
+
+	public void setFirstRow(Integer firstRow) {
+		this.firstRow = firstRow;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getCityId() {
+		return cityId;
+	}
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getLabelName() {
+		return labelName;
+	}
+	public void setLabelName(String labelName) {
+		this.labelName = labelName;
+	}
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getBannerUrl() {
+		return bannerUrl;
+	}
+	public void setBannerUrl(String bannerUrl) {
+		this.bannerUrl = bannerUrl;
+	}
+	public Integer getSerial() {
+		return serial;
+	}
+	public void setSerial(Integer serial) {
+		this.serial = serial;
+	}
+	
+}
---
+++ src/main/java/com/imzhitu/admin/common/pojo/OpNearLabelWorldDto.java
@@ -0,0 +1,37 @@
+package com.imzhitu.admin.common.pojo;
+
+
+public class OpNearLabelWorldDto extends ZTWorldDto{
+
+	private static final long serialVersionUID = -5124380453654977751L;
+	private Integer nearLabelId;
+	private String nearLabelName;
+	private Integer serial;
+	private Integer nearLabelWorldId;
+	public Integer getNearLabelId() {
+		return nearLabelId;
+	}
+	public void setNearLabelId(Integer nearLabelId) {
+		this.nearLabelId = nearLabelId;
+	}
+	public String getNearLabelName() {
+		return nearLabelName;
+	}
+	public void setNearLabelName(String nearLabelName) {
+		this.nearLabelName = nearLabelName;
+	}
+	public Integer getSerial() {
+		return serial;
+	}
+	public void setSerial(Integer serial) {
+		this.serial = serial;
+	}
+	public Integer getNearLabelWorldId() {
+		return nearLabelWorldId;
+	}
+	public void setNearLabelWorldId(Integer nearLabelWorldId) {
+		this.nearLabelWorldId = nearLabelWorldId;
+	}
+	
+
+}
